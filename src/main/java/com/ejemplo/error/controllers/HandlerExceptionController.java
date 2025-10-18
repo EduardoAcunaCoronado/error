1b1bcaeec77cb6d4b5c4c0bb34c09b8ba4421676
eduardoacunacoronado <edu191191@gmail.com>
2025-10-18 21:33:38 +0200

    Personalizar error 404.

--- src/main/java/com/ejemplo/error/controllers/HandlerExceptionController.java
+++ src/main/java/com/ejemplo/error/controllers/HandlerExceptionController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.Date;
 
@@ -22,4 +23,13 @@ public class HandlerExceptionController {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
     }
 
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> noHandlerFound(NoHandlerFoundException e) {
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setMessage(e.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setError("No Handler found");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
+    }
 }
