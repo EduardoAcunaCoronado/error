bf6c1c495d7fb7ddd80a67b0934dcd8170571fbf
eduardoacunacoronado <edu191191@gmail.com>
2025-10-18 22:21:22 +0200

    NumberFormatException.

--- src/main/java/com/ejemplo/error/controllers/HandlerExceptionController.java
+++ src/main/java/com/ejemplo/error/controllers/HandlerExceptionController.java
@@ -4,10 +4,13 @@ import com.ejemplo.error.models.Error;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class HandlerExceptionController {
@@ -21,6 +24,17 @@ public class HandlerExceptionController {
         error.setError("Error division by zero");
 //        return ResponseEntity.internalServerError().body(error);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+    }
+
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, Object> numberFormat(Exception e) {
+        Map<String, Object> error = new HashMap<>();
+        error.put("date", new Date());
+        error.put("message", e.getMessage());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        error.put("error", "Formato de número inválido");
+        return error;
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
